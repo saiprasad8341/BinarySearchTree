4dd34a84391b96adaee8825b4e785b17d3765f83
Saiprasad8341 <saiprasad.8341@gmail.com>
2022-11-25 19:47:16 +0530

    UC1 - Creating an Binary Search Tree

--- src/com/bridgelab/BinarySearchTree.java
+++ src/com/bridgelab/BinarySearchTree.java
@@ -1,7 +1,41 @@
 package com.bridgelab;
 
+//UC1 - Ability to create a BST by adding 56 and then adding 30 & 70
+
 public class BinarySearchTree {
+
+    private TreeNode root;
+
+    private class TreeNode{
+        private int data;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(int data){
+            this.data = data;
+        }
+    }
+
+    public void insert(int value){ root = insert(root, value);}
+
+    public TreeNode insert(TreeNode root, int value){
+        if(root == null || root.data == value){
+            root = new TreeNode(value);
+            return root;
+        }
+        if (value < root.data){
+            root.left = insert(root.left, value);
+        }else {
+            root.right = insert(root.right, value);
+        }
+        return root;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to the Binary Search Tree (BST) Problems By Using Java");
+
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.insert(56);
+        bst.insert(30);
+        bst.insert(70);
     }
 }
