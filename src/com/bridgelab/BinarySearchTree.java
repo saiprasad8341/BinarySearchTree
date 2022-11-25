47b3ec79a851ddd78024550a6f7c2b09ea9aa07e
Saiprasad8341 <saiprasad.8341@gmail.com>
2022-11-25 16:13:02 +0530

    Welcome to the Binary Search Tree (BST)

---
+++ src/com/bridgelab/BinarySearchTree.java
@@ -0,0 +1,7 @@
+package com.bridgelab;
+
+public class BinarySearchTree {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Binary Search Tree (BST) Problems By Using Java");
+    }
+}
